1dd3c437a66af13d9bb149a2270d729dcc87814f
Alissa Girdler <alissa.girdler@gmail.com>
2024-04-28 13:05:09 -0500

    Level 1

--- textonly.py
+++ textonly.py
@@ -97,12 +97,42 @@ def Level_1():
         print(Level_1_Storyline["Level_1_Girls_Option_Response"])
 
     elif("exit" in Level_1_Option.lower()):
-        print(Level_1_Storyline["Exit"])
+        print(Misc_Storyline["Exit"])
 
     else:
-        print(Level_1_Storyline["Invalid_Entry"])
+        print(Misc_Storyline["Invalid_Entry"])
 
     return Level_1_Option
 
-print(Misc_Storyline["Introduction"])
+def Level_2():
+    Level_1_Option = Level_1()
+     
+    if int(Level_1_Option) == 1:
+        Level_2_Option = input(Level_2_Storyline["Level_2_Solo_Domestic/International_Option"])
+
+        if Level_2_Option == 1:
+            print(Level_2_Storyline["Level_2_Solo_Domestic_Response"])
+
+        elif Level_2_Option == 2:
+            print(Level_2_Storyline["Level_2_Solo_International_Response"])
+        
+        elif("exit" in Level_2_Option.lower()):
+            print(Misc_Storyline["Exit"])
+
+        else:
+            print(Misc_Storyline["Invalid_Entry"])
+
+        return Level_2_Option
+
+    elif int(Level_1_Option) == 2:
+        
+
+    elif int(Level_1_Option) == 3:
+        print(Level_1_Storyline["Level_1_Girls_Option_Response"])
+
+    elif int(Level_1_Option) == 4:
+
+    
+
+
 Level_1()
